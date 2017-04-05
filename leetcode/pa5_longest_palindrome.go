00f2dc89ac7e870532308045c85ce8ccc739fcbb
home3k <home3k@gmail.com>
2017-04-05 17:47:39 +0800

    pa5-8

--- leetcode/pa5_longest_palindrome.go
+++ leetcode/pa5_longest_palindrome.go
@@ -1,5 +1,54 @@
 package main
 
+import "fmt"
+
+// abc
 func longestPalindrome(s string) string {
-	return ""
+	if len(s) <=1 {
+		return s
+	}
+	ns := "#"
+	var rl []int = []int{1}
+	for _, r := range s {
+		ns += string(r) + "#"
+		rl = append(rl,1,1)
+	}
+	var max, maxRight, pos, maxPos int
+	for i := range ns {
+		if i < maxRight {
+			min := maxRight - i
+			if rl[pos-(i-pos)] < min {
+				min = rl[pos-(i-pos)]
+			}
+			rl[i] = min
+		} else {
+			rl[i] = 1
+		}
+		// check
+		for i-rl[i] >= 0 && i+rl[i] < len(ns) && ns[i-rl[i]] == ns[i+rl[i]] {
+			rl[i]++
+		}
+		if rl[i]+i-1 > maxRight {
+			maxRight = rl[i] + i - 1
+			pos = i
+		}
+		if max < rl[i]  {
+			max = rl[i]
+			maxPos = i
+		}
+	}
+	ns = ns[maxPos - max + 1: maxPos + max -1]
+	result:=""
+	for _, r := range ns {
+		if string(r)!="#" {
+			result += string(r)
+		}
+	}
+	return result
+}
+
+func main()  {
+	fmt.Println(longestPalindrome("abcdc"))
+	fmt.Println(longestPalindrome(""))
+	fmt.Println(longestPalindrome("abcdsdcbsdeddf"))
 }
--- leetcode/pa8_atoi.go
+++ leetcode/pa8_atoi.go
@@ -1,5 +1,62 @@
 package main
 
-func myAtoi(str string) int {
+import "fmt"
 
+const (
+	INT_MAX = 2147483647
+)
+
+func myAtoi(str string) int {
+	if str == "" {
+		return 0
+	}
+	positive := true
+	first := true
+	result := 0
+	for _, r := range str {
+		if first {
+			if r == ' ' || r == '\r' {
+				continue
+			}
+			if r == '-' {
+				positive = false
+			} else if r == '+' || (r >= '0' && r <= '9') {
+				positive = true
+				if r != '+' {
+					result = int(r - '0')
+				}
+			} else {
+				return 0
+			}
+			first = false
+		} else {
+			if r >= '0' && r <= '9' {
+				result = result*10 + int(r-'0')
+				// overflow
+				if positive && result >= INT_MAX  {
+					return INT_MAX
+				}
+				if !positive && result >= (INT_MAX+1) {
+					return -(INT_MAX + 1)
+				}
+			} else {
+				break
+			}
+		}
+
+	}
+	if !positive {
+		result = -result
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(myAtoi(""))
+	fmt.Println(myAtoi("-1043dx4444222323dasdfadfasdf00"))
+	fmt.Println(myAtoi("dd"))
+	fmt.Println(myAtoi("+100"))
+	fmt.Println(myAtoi("         +13c00"))
+	fmt.Println(myAtoi("   +0 123"))
+	fmt.Println(myAtoi("2147483648"))
 }
