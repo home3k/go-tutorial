16a601f30b2f819a80f5c86b624b013f6da9aebf
home3k <home3k@gmail.com>
2017-04-06 19:17:24 +0800

    pa22

--- leetcode/pa22_gen_parentheses.go
+++ leetcode/pa22_gen_parentheses.go
@@ -9,37 +9,34 @@ import "fmt"
 //"()(())",
 //"()()()"
 //]
+// (  )
+// x x x | x x x
+// xx ( xxx
+// x((xxx
+// (((xxx
+//
 func generateParenthesis(n int) []string {
 	if n == 0 {
 		return []string{}
 	}
-	if n == 1 {
-		return []string{"()"}
-	}
-	var result []string
-	var contain = make(map[string]bool)
-	var s string
-	for _, p:=range generateParenthesis(n-1) {
-		s = "("+ p +")"
-		if _, ok :=contain[s];!ok {
-			result = append(result, s)
-			contain[s] = true
-		}
-		s = p + "()"
-		if _, ok :=contain[s];!ok {
-			result = append(result, s)
-			contain[s] = true
-		}
-		s = "()" + p
-		if _, ok :=contain[s];!ok {
-			result = append(result, s)
-			contain[s] = true
-		}
-	}
+	var result []string = []string{}
+	gen(&result, "", n, n)
 	return result
 }
 
-func main()  {
+func gen(result *[]string, p string, left int, right int) {
+	if left == 0 && right == 0 {
+		*result = append(*result, p)
+	}
+	if left > 0 {
+		gen(result, p+"(", left-1, right)
+	}
+	if right > 0 && right>left{
+		gen(result, p+")", left, right-1)
+	}
+}
+
+func main() {
 	fmt.Println(generateParenthesis(3))
 	fmt.Println(generateParenthesis(1))
 	fmt.Println(generateParenthesis(0))
