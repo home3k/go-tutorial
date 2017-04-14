db4e3e31ee8e858b569f0f7c4109097320c4875a
home3k <home3k@gmail.com>
2017-04-14 18:14:52 +0800

    pa41

---
+++ leetcode/pa15_3sum.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+
+For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+-4 -1 -1 0 1 2
+
+
+
+ */
+func threeSum(nums []int) [][]int {
+
+}
+
---
+++ leetcode/pa41_first_missing_positive.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func firstMissingPositive(nums []int) int {
+	for i, num:=range nums {
+		if num>0 && num<=len(nums) && num!=i+1{
+			nums[num-1]=num
+		}
+	}
+
+	for i, num:=range nums {
+		if i+1!=num {
+			return i+1
+		}
+	}
+	return len(nums)+1
+}
+
+func main()  {
+	fmt.Println(firstMissingPositive([]int{1,2,3,4}))
+	fmt.Println(firstMissingPositive([]int{1,2,0}))
+	fmt.Println(firstMissingPositive([]int{3,4,-1,1}))
+}
