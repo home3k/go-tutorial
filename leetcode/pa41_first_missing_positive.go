94ff815a7206ab9595539ba0bdf8f193428762b9
home3k <home3k@gmail.com>
2017-04-14 18:18:37 +0800

    pa41

--- leetcode/pa41_first_missing_positive.go
+++ leetcode/pa41_first_missing_positive.go
@@ -5,7 +5,7 @@ import "fmt"
 func firstMissingPositive(nums []int) int {
 	for i, num:=range nums {
 		if num>0 && num<=len(nums) && num!=i+1{
-			nums[num-1]=num
+			nums[num-1], nums[i]=nums[i],nums[num-1]
 		}
 	}
 
@@ -21,4 +21,5 @@ func main()  {
 	fmt.Println(firstMissingPositive([]int{1,2,3,4}))
 	fmt.Println(firstMissingPositive([]int{1,2,0}))
 	fmt.Println(firstMissingPositive([]int{3,4,-1,1}))
+	fmt.Println(firstMissingPositive([]int{2,1}))
 }
