92097da7f2356e064899a363ce7510c9937fb241
home3k <home3k@gmail.com>
2017-05-10 22:26:00 +0800

    fix

--- leetcode/pa37_sudoku_solver.go
+++ leetcode/pa37_sudoku_solver.go
@@ -1,1 +1,6 @@
 package main
+
+
+func solveSudoku(board [][]byte)  {
+
+}
--- leetcode/pa39_combination_sum.go
+++ leetcode/pa39_combination_sum.go
@@ -1,1 +1,76 @@
 package main
+
+/*
+Given a set of candidate numbers (C) (without duplicates) and a target number (T),
+find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+For example, given candidate set [2, 3, 6, 7] and target 7,
+
+A solution set is:
+[
+  [7],
+  [2, 2, 3]
+]
+ */
+func combinationSum(candidates []int, target int) [][]int {
+	if len(candidates) == 0 {
+		return [][]int{}
+	}
+	result:=[][]int{}
+	sort(candidates, 0, len(candidates)-1)
+	if search(candidates, target, 0, len(candidates)) {
+		result = append(result, []int{target})
+	}
+	start, end := 0, len(candidates)
+	for start < end {
+		if candidates[start] >= target {
+			break
+		}
+
+	}
+}
+
+func search(nums[]int, target int, start int, end int) bool {
+	if len(nums) == 0 {
+		return false
+	}
+	if len(nums) == 1 {
+		return nums[0]==target
+	}
+	half:=len(nums)/2
+	if nums[half] == target {
+		return true
+	} else if nums[half] > target {
+		return search(nums, target, start, half)
+	} else {
+		return search(nums, target, half+1, end)
+	}
+}
+
+func sort(nums[]int, start int, end int) {
+	if start >= end {
+		return
+	}
+	pivot := nums[(start+end)/2]
+	left, right:=start, end
+	for left <= right {
+		for left <= right && nums[left] <= pivot  {
+			left++
+		}
+		for left <= right && nums[right] >= pivot {
+			right--
+		}
+		if left <= right {
+			nums[left], nums[right] = nums[right], nums[left]
+			left++;right--
+		}
+	}
+	sort(nums, start, right)
+	sort(nums, left, end)
+}
