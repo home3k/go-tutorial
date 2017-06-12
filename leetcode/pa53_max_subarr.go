9003716762ffe5154951ef920ff54d4647acfad0
home3k <home3k@gmail.com>
2017-06-12 15:21:24 +0800

    pa53

--- leetcode/pa53_max_subarr.go
+++ leetcode/pa53_max_subarr.go
@@ -1,4 +1,7 @@
 package main
+
+import "fmt"
+
 /*
 
 For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
@@ -8,6 +11,51 @@ the contiguous subarray [4,-1,2,1] has the largest sum = 6.
 
  */
 
-func maxSubArray(nums []int) int {
 
+const (
+	INT_MIN = -2147483648
+)
+
+
+func maxSubArray(nums []int) int {
+	if len(nums)==0 {
+		return 0
+	}
+	max, sum, current, next:=INT_MIN, 0, 0, -1
+	for i:=0;i<len(nums); {
+		v:=nums[i]
+		sum=v;current=i;next=-1
+		if sum < max {
+			continue
+		} else {
+			max = sum
+		}
+		j:=i+1
+		for j<len(nums) {
+			vj:=nums[j]
+			if vj>=0 {
+				sum += vj
+				max = sum
+				if vj > nums[current] {
+					if (next >=0 && nums[next]<vj) || next<0{
+						next = j
+					}
+				}
+				j++
+			} else {
+				if next>=0 {
+					i=next
+				} else {
+					i=j+1
+				}
+				break
+			}
+		}
+
+	}
+	return max
+}
+
+func main()  {
+	fmt.Println(maxSubArray([]int{-2,1,-3,4,-1,2,1,-5,4}))
 }
